ab1da7b5ac06a52710280e15ca92d2359162f9a5
TUANANHLC <51194764+tuananhkgasd7@users.noreply.github.com>
2022-04-11 06:47:32 +0700

    done Login

--- Management_Book/MainWindow.xaml.cs
+++ Management_Book/MainWindow.xaml.cs
@@ -71,141 +71,17 @@ namespace Management_Book
             public string HeaderText { get; set; }
             public UserControl Content { get; set; }
         }
-
-        class AppConfig
-        {
-            public static string Server = "Server";
-            public static string Database = "Database";
-            public static string Username = "Username";
-            public static string Password = "Password";
-            public static string Entropy = "Entropy";
-            public static string GetValue(string key)
-            {
-                string value = ConfigurationManager.AppSettings[key];
-                return value;
-            }
-
-            public static void SetValue(string key, string value)
-            {
-                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-                var settings = configFile.AppSettings.Settings;
-                settings[key].Value = value;
-
-                configFile.Save(ConfigurationSaveMode.Minimal);
-            }
-
-            public static string ConnectionString()
-            {
-                string result = "";
-
-                var builder = new SqlConnectionStringBuilder();
-                string server = AppConfig.GetValue(AppConfig.Server);
-                string database = AppConfig.GetValue(AppConfig.Database);
-                string username = AppConfig.GetValue(AppConfig.Username);
-                string password = AppConfig.GetValue(AppConfig.Password);
-
-                builder.DataSource = $"{server}";
-                builder.InitialCatalog = database;
-                builder.IntegratedSecurity = true;
-                builder.ConnectTimeout = 6;
-
-                result = builder.ToString();
-                return result;
-            }
-        }
-
-        class SqlDataAccess
-        {
-            private SqlConnection _connection;
-            public SqlDataAccess(string connectionString)
-            {
-                _connection = new SqlConnection(connectionString);
-            }
-
-            public bool CanConnect()
-            {
-                bool result = true;
-
-                try
-                {
-                    _connection.Open();
-                    _connection.Close();
-                }
-                catch (Exception ex)
-                {
-                    result = false;
-                }
-
-                return result;
-            }
-
-            public void Connect()
-            {
-                _connection.Open();
-            }
-        }
         private void ThemedWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            if (Frm_Login.IsActive())
-            {
-                var screen = new ObservableCollection<DXTabItem>()
+
+            var screen = new ObservableCollection<DXTabItem>()
                 {
                     new DXTabItem{Content = new MasterDataUserControl(), Header = "MasterData"},
                     new DXTabItem{Content = new SaleUserControl(), Header = "Sale"},
                     new DXTabItem{Content = new OrderUserControl(), Header = "Order"}
                 };
 
-                dXTabControl1.ItemsSource = screen;
-            }
-            else
-                System.Windows.Application.Current.Shutdown();
-            //Kết nối db
-            //string connectionString = AppConfig.ConnectionString();
-            //var dao = new SqlDataAccess(connectionString);
-
-            //if (dao.CanConnect())
-            //{
-            //    dao.Connect();
-            //    Title = "Connected";
-            //    MessageBox.Show("Connected");
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Cannot connect to database");
-            //}
-
-            //Đọc password
-            //var cypherText = AppConfig.GetValue(AppConfig.Password);
-            //var cypherTextInBytes = Convert.FromBase64String(cypherText);
-
-            //var entropyText = AppConfig.GetValue(AppConfig.Entropy);
-            //var entropyInBytes = Convert.FromBase64String(entropyText);
-
-            //var passwordInBytes = ProtectedData.Unprotect(cypherTextInBytes, entropyInBytes, DataProtectionScope.CurrentUser);
-            //var password = Encoding.UTF8.GetString(passwordInBytes);
-            //MessageBox.Show(password);
-        }
-
-        private void saveButton_Click(object sender, RoutedEventArgs e)
-        {
-            string password = "012345";
-
-            var passwordInBytes = Encoding.UTF8.GetBytes(password);
-
-            var entropy = new byte[20];
-
-            using (var rng = new RNGCryptoServiceProvider())
-            {
-                rng.GetBytes(entropy);
-            }
-            var entropyBase64 = Convert.ToBase64String(entropy);
-
-            var cypherText = ProtectedData.Protect(passwordInBytes, entropy, DataProtectionScope.CurrentUser);
-
-            var cypherTextBase64 = Convert.ToBase64String(cypherText);
-
-            AppConfig.SetValue(AppConfig.Password, cypherTextBase64);
-            AppConfig.SetValue(AppConfig.Entropy, entropyBase64);
+            dXTabControl1.ItemsSource = screen;
         }
     }
 }
--- Management_Book/Views/Frm_Login.xaml.cs
+++ Management_Book/Views/Frm_Login.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using DevExpress.Xpf.Core;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
@@ -26,19 +27,89 @@ namespace Management_Book.Views
         {
             InitializeComponent();
         }
+        class AppConfig
+        {
+            public static string Server = "Server";
+            public static string Instance = "Instance";
+            public static string Database = "Database";
+            public static string getValue(string key)
+            {
+                string value = (string)ConfigurationManager.AppSettings[key];
+                return value;
+            }
+            public static void setValue(string key, string value)
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                settings[key].Value = value;
 
+                configFile.Save(ConfigurationSaveMode.Minimal);
+            }
+            public static string ConnectionString()
+            {
+                string result = "";
+
+                var builder = new SqlConnectionStringBuilder();
+                string server = AppConfig.getValue(AppConfig.Server);
+                string instance = AppConfig.getValue(AppConfig.Instance);
+                string database = AppConfig.getValue(AppConfig.Database);
+
+                //builder.DataSource = $"{server}\\{instance}";
+                builder.DataSource = $"{instance}";
+                builder.InitialCatalog = database;
+                builder.IntegratedSecurity = true;
+                builder.ConnectTimeout = 5;
+
+                result = builder.ToString();
+                return result;
+            }
+        }
+
+
+        class SqlDataAccess
+        {
+            private SqlConnection _connection;
+            public SqlDataAccess(string connectionString)
+            {
+                _connection = new SqlConnection(connectionString);
+            }
+
+            public bool CanConnect()
+            {
+                bool result = true;
+
+                try
+                {
+                    _connection.Open();
+                    _connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    result = false;
+                    MessageBox.Show(ex.Message);
+                }
+
+                return result;
+            }
+
+            public void Connect()
+            {
+                _connection.Open();
+            }
+        }
         private void BtnOK_Click(object sender, RoutedEventArgs e)
         {
-            SqlConnection sqlCon = new SqlConnection(@"Data Source=localhost\DESKTOP-95BV1QQ; Initial Catalog=MyShop; Integrated Security=True;");
+            string connectionString = AppConfig.ConnectionString();
+            SqlConnection sqlCon = new SqlConnection(connectionString);
             try
             {
                 if (sqlCon.State == ConnectionState.Closed)
                     sqlCon.Open();
-                String query = "SELECT COUNT(1) FROM User WHERE Username=@Username AND Password=@Password";
+                String query = "SELECT COUNT(1) FROM UserInformation WHERE Username=@Username AND Password=@Password";
                 SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
                 sqlCmd.CommandType = CommandType.Text;
-                sqlCmd.Parameters.AddWithValue("@Username", TxtUsername);
-                sqlCmd.Parameters.AddWithValue("@Password", TxtPassword);
+                sqlCmd.Parameters.AddWithValue("@Username", TxtUsername.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", TxtPassword.Password);
                 int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
                 if (count == 1)
                 {
@@ -48,7 +119,7 @@ namespace Management_Book.Views
                 }
                 else
                 {
-                    MessageBox.Show("Username or password is incorrect.");
+                    MessageBox.Show("Username or password is incorrect.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
             catch (Exception ex)
@@ -59,6 +130,16 @@ namespace Management_Book.Views
             {
                 sqlCon.Close();
             }
+        }
+
+        private void SimpleButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Regis(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
